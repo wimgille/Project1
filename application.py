03e98209d72ecfd495e919c7ce5de33184c7db2a
wimgille <wimgille@MacBook-Pro-van-Wim.local>
2019-12-20 12:53:29 +0100

    Finalized, registration, log in and log out

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+# from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -20,7 +22,105 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/page1")
+def more():
+    return render_template("page1.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register new user including password"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("apology.html", message="must provide username"), 400
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("apology.html", message="must provide password"), 400
+
+        # Ensure password confirmation was submitted match
+        elif not request.form.get("confirmation"):
+            return render_template("apology.html", message="please confirm password"), 400
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username": request.form.get("username")}).fetchall()
+
+        # Ensure username does not exist
+        if len(rows) == 1:
+            return render_template("apology.html", message="user already exists"), 400
+
+        # Check if the password and the confirmation password match
+        if request.form.get("password") != request.form.get("confirmation"):
+            return render_template("apology.html", message="Password and confirmation dont match"), 400
+
+        pw_hash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+        db.execute("INSERT INTO users (username,hash) VALUES (:name,:hash_value)",
+                   {"name": request.form.get("username"),
+                   "hash_value": pw_hash})
+
+        db.commit()
+
+        # Redirect user to success page
+        return render_template("success.html", message="Well done")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("apology.html", message="must provide username"), 400
+ 
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("apology.html", message="must provide password"), 400
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username": request.form.get("username")}).fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("apology.html", message="invalid username and/or password"), 400
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["user_id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
